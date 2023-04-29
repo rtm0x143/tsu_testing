401d7cdf22bfcb57a6a58caedf6cccd44f6b49c4
Artyom Ebel <eartyom323@gmail.com>
2023-04-29 21:27:41 +0700

    feat(Api): controller

--- SolutionApi/Program.cs
+++ SolutionApi/Program.cs
@@ -2,11 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.MapControllers();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
---
+++ SolutionApi/SolutionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolutionApi;
+
+[ApiController]
+public class SolutionController : ControllerBase
+{
+    public record CallCombinationSumCommand(int[] Candidates, int Target);
+    
+    [HttpPost("combination-sum")]
+    public IList<IList<int>> Post(CallCombinationSumCommand command)
+    {
+        return new Solution().CombinationSum(command.Candidates, command.Target);
+    }
+}
