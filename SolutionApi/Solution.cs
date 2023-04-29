ea95d0adac6f12bb58885acb83e8ee85b4b893b0
Artyom Ebel <eartyom323@gmail.com>
2023-04-29 21:26:52 +0700

    refactor(Solution): code style

---
+++ SolutionApi/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace SolutionApi;
+
+public class Solution
+{
+    private readonly IList<IList<int>> _result = new List<IList<int>>();
+
+    private void _backtrack(int index, List<int> path, int total, int[] candidates, int target)
+    {
+        if (total == target)
+        {
+            _result.Add(path.ToList());
+            return;
+        }
+
+        if (total > target || index >= candidates.Length) return;
+
+        path.Add(candidates[index]);
+        _backtrack(index,
+            path,
+            total + candidates[index],
+            candidates,
+            target);
+
+        path.Remove(path.Last());
+
+        _backtrack(index + 1,
+            path,
+            total,
+            candidates,
+            target);
+    }
+
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        _backtrack(0, new List<int>(), 0, candidates, target);
+        return _result;
+    }
+}
